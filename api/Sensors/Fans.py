fdbed39804bf6f2b350cdeff5430501c7446b5b0
Aske Merci <rainman1011@tuta.io>
2021-07-11 18:39:10 +0200

    closes #24 implemented get_fan_rpm

--- api/Sensors/Fans.py
+++ api/Sensors/Fans.py
@@ -5,3 +5,8 @@ def get_num_fans():
     if len(psutil.sensors_fans()) == 0:
         return False
     return len(psutil.sensors_fans())
+
+
+def get_fan_rpm(fan):
+    fans = psutil.sensors_fans()
+    return fans[fan][0].current
