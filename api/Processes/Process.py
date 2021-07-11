7efba30c7b6598e87962e56157efca39d8d50145
Aske Merci <rainman1011@tuta.io>
2021-07-11 13:06:00 +0200

    implemented Proccess class

---
+++ api/Processes/Process.py
@@ -0,0 +1,45 @@
+import psutil
+
+
+def get_processes():
+    processes = list()
+    for proc in psutil.process_iter(['name', 'pid', 'username', 'status']):
+        processes.append(proc.info)
+    return processes
+
+
+def get_pids():
+    return psutil.pids()
+
+
+def get_processes_by_username(username):
+    processes = list()
+    for proc in psutil.process_iter(['name', 'pid', 'username', 'status']):
+        if proc.info["username"] == username:
+            processes.append(proc.info)
+    return processes
+
+
+def get_process_by_name(name):
+    processes = list()
+    for proc in psutil.process_iter(['name', 'pid', 'username', 'status']):
+        if proc.info["name"] == name:
+            processes.append(proc.info)
+    return processes
+
+
+def get_process_by_pid(pid):
+    for proc in psutil.process_iter(['name', 'pid', 'username', 'status']):
+        if proc.info["pid"] == pid:
+            return proc.info
+
+
+def get_processes_by_status(status):
+    processes = list()
+    for proc in psutil.process_iter(['name', 'pid', 'username', 'status']):
+        if proc.info["status"] == status:
+            processes.append(proc.info)
+    return processes
+
+
+print(get_process_by_pid(19))
