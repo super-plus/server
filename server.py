5579f8a8202c0da2a2019ceab5007cace52a3cca
Aske Merci <rainman1011@tuta.io>
2021-07-15 19:52:05 +0200

    made routes for os

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import flask
-from config import server, requests
+from config import server, api
+from routes.OS.Routes import os_routes
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = server["DEBUG"]
@@ -8,6 +9,9 @@ app.config["DEBUG"] = server["DEBUG"]
 @app.route('/', methods=['GET'])
 def root():
     return '''<h1> <3 </h1>'''
+
+
+app.register_blueprint(os_routes, url_prefix=api["BASE"] + 'os')
 
 
 app.run(host=server["HOST"], port=server["PORT"])
