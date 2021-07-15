af2031f08735fd41964f412d870b3465688b6065
Aske Merci <rainman1011@tuta.io>
2021-07-15 20:12:09 +0200

    whoops forgot to init file

--- server.py
+++ server.py
@@ -3,6 +3,8 @@ from config import server, api
 from routes.OS.Routes import os_routes
 from routes.User.Routes import user_routes
 from routes.Sensors.Routes import sensors_routes
+from routes.Drives.Routes import drives_routes
+
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = server["DEBUG"]
@@ -16,6 +18,7 @@ def root():
 app.register_blueprint(os_routes, url_prefix=api["BASE"] + 'os')
 app.register_blueprint(user_routes, url_prefix=api["BASE"] + 'users')
 app.register_blueprint(sensors_routes, url_prefix=api["BASE"] + 'sensors')
+app.register_blueprint(drives_routes, url_prefix=api["BASE"] + 'drives')
 
 
 app.run(host=server["HOST"], port=server["PORT"])
