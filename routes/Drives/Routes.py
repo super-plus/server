a768fa3a9ce1a44ff9dd97eafa92891ea2644cef
Aske Merci <rainman1011@tuta.io>
2021-07-15 20:10:57 +0200

    made routes for drives

---
+++ routes/Drives/Routes.py
@@ -0,0 +1,14 @@
+from flask import Blueprint, jsonify
+from api.Drives import Partitions
+
+drives_routes = Blueprint('drives', __name__)
+
+
+@drives_routes.route('/get')
+def get_drives():
+    return jsonify(Partitions.get_all())
+
+
+@drives_routes.route('/get/<drive>')
+def get_num_fans(drive):
+    return jsonify(Partitions.get_drive(drive))
