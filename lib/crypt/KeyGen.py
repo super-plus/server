b80113a8014da8c74a3feb86593bd13f4ddc55cc
Aske Merci <rainman1011@tuta.io>
2021-07-17 18:42:19 +0200

    closes #55

--- lib/crypt/KeyGen.py
+++ lib/crypt/KeyGen.py
@@ -59,10 +59,13 @@ class KeyGen:
                                 usage={KeyFlags.Sign,
                                        KeyFlags.EncryptCommunications,
                                        KeyFlags.EncryptStorage},
+
                                 hashes=[HashAlgorithm.SHA256, HashAlgorithm.SHA384, HashAlgorithm.SHA512,
                                         HashAlgorithm.SHA224],
-                                ciphers=[SymmetricKeyAlgorithm.AES256, SymmetricKeyAlgorithm.AES192,
+                                ciphers=[SymmetricKeyAlgorithm.AES256,
+                                         SymmetricKeyAlgorithm.AES192,
                                          SymmetricKeyAlgorithm.AES128],
+
                                 compression=[CompressionAlgorithm.ZLIB, CompressionAlgorithm.BZ2,
                                              CompressionAlgorithm.ZIP, CompressionAlgorithm.Uncompressed],
                                 key_expiration=timedelta(days=self.exp))
