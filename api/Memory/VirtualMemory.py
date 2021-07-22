d3aabd5ce42cbc7a7925132e31a115f3d085529e
Aske Merci <rainman1011@tuta.io>
2021-07-22 21:51:54 +0200

    parsed it so it works with keys

--- api/Memory/VirtualMemory.py
+++ api/Memory/VirtualMemory.py
@@ -3,11 +3,25 @@ from api.Utils import BytesUtil
 
 
 def get_virtual_memory():
-    return psutil.virtual_memory()
+    mem = psutil.virtual_memory()
+    return {
+        "total": mem.total,
+        "available": mem.available,
+        "percent": mem.percent,
+        "used": mem.used,
+        "free": mem.free,
+        "active": mem.active,
+        "inactive": mem.inactive,
+        "buffers": mem.buffers,
+        "cached": mem.cached,
+        "shared": mem.shared,
+        "slab": mem.slab,
+        "status": get_virtual_memory_status()
+    }
 
 
 def get_virtual_memory_status():
-    available = BytesUtil.__b_to_mb(get_virtual_memory().available)
+    available = BytesUtil.__b_to_mb(psutil.virtual_memory().available)
     if available > (available / 10):
         return True
     return False
