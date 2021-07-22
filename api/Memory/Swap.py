2da5b11e252aa2bec5f911149d0da2c2cc3b3d2d
Aske Merci <rainman1011@tuta.io>
2021-07-22 21:54:30 +0200

    parsed it so it works with keys

--- api/Memory/Swap.py
+++ api/Memory/Swap.py
@@ -3,11 +3,20 @@ from api.Utils import BytesUtil
 
 
 def get_swap_memory():
-    return psutil.swap_memory()
+    swap = psutil.swap_memory()
+    return {
+        "total": swap.total,
+        "used": swap.used,
+        "free": swap.free,
+        "percent": swap.percent,
+        "sin": swap.sin,
+        "sout": swap.sout,
+        "status": get_swap_memory_status()
+    }
 
 
 def get_swap_memory_status():
-    available = BytesUtil.__b_to_mb(get_swap_memory().available)
+    available = BytesUtil.__b_to_mb(psutil.swap_memory().free)
     if available > (available / 10):
         return True
     return False
